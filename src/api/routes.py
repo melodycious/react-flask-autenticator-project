2b3d95967b042054c123a22ea356748fd39e2805
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-05-06 18:21:44 +0000

    creado token

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,28 +1,37 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
+import os 
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager, create_access_token
 
 api = Blueprint('api', __name__)
 
-app.config["JWT_SECRET_KEY"] = "super-secret" 
+# Create Flask application instance
+app = Flask(__name__)
+
+# Configure JWT secret key
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
+
+# Initialize CORS extension
+CORS(app)
+
+# Initialize JWT extension
 jwt = JWTManager(app)
 
-# Allow CORS requests to this API
-CORS(api)
+# Route to authenticate users and return JWTs
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
 
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
+# Sample route
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
