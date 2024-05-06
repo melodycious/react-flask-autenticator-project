c0ca7ba9bceb2d2e9cae801491b5bcec68299c4c
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-05-06 16:26:26 +0000

    importando jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,14 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
+
+app.config["JWT_SECRET_KEY"] = "super-secret" 
+jwt = JWTManager(app)
 
 # Allow CORS requests to this API
 CORS(api)
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,7 +8,7 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Pruebas de autentificación</h1>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
