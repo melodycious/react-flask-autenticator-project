b1c304d8cb3302a6cfe757c5909470cbe93a6320
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-05-07 12:59:59 +0000

    refrescar automatico

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,45 +2,42 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Router, useNavigate } from "react-router-dom";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const token = localStorage.getItem("token");
+	const navigate = useNavigate();
+	console.log("este es el token", token)
 	
 	const handleClick = () => {
-
-		const opts = {
-			method:'POST',
-			headers: {
-				"content-type": "application/json"
-			},
-			body: JSON.stringify(
-				{
-					"email": email,
-					"password": password
-				}
-			)
-		}
-		fetch('https://turbo-xylophone-q77x9v5vqx4p3qg5-3001.app.github.dev/api/token', opts)
+		actions.login(email, password)/* .then(() => {
+			navigate.push("/")
 		
-			.then(resp => {
-				if (resp.status === 200 ) return resp.json();
-				else alert("There has been an error");
-			})
-			.then()
-			.catch(error =>{
-				console.error("there was an error!!!!", error);
-			})
-	}
+		}); */
+	};
 
 
 	return (
+
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			<input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-			<input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-			<button onClick={handleClick}>Login</button>
-		</div>
-	);
+			{token && token!="" && token!=undefined ? (
+				"you are logged in with this token" + token
+			) : (
+				<div>
+				<input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+				<input type="password" 
+					   placeholder="password" 
+					   value={password} 
+					   onChange={(e) => setPassword(e.target.value)} />
+				<button onClick={handleClick}>Login</button>
+				</div>
+				)
+			}
+				</div>
+			
+	)
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -17,38 +18,43 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
+			login: async (email, password) => {
+				const opts = {
+					method:'POST',
+					headers: {
+						"content-type": "application/json"
+					},
+					body: JSON.stringify(
+						{
+							"email": email,
+							"password": password
+						}
+					)
+				}
+
+				try {
+					const resp = await fetch('https://turbo-xylophone-q77x9v5vqx4p3qg5-3001.app.github.dev/api/token', opts)
+
+					if (resp.status !== 200 ) {
+						alert("There has been an error");
+						return false;
+					}
+
+					const data = await resp.json();
+						console.log("from back", data)
+						localStorage.setItem("token", data.access_token);
+						setStore({token: data.access_token});
+						return true;
+				}
+
+				catch(error){
 					console.log("Error loading message from backend", error)
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
+			
 		}
-	};
+	}
 };
 
 export default getState;
